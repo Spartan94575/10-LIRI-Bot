9fa6ff7487cb63ec9bc52974f8c659e63321da74
Michael Sendle <michael.sendle@icloud.com>
2019-05-18 17:43:05 -0700

    whating for api keys and need a littel help

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+//"node-spotify-api": {
+    //"version": "1.1.1",
+    //"resolved": "https://registry.npmjs.org/node-spotify-api/-/node-spotify-api-1.1.1.tgz",
+    //"integrity": "sha512-7TUox594clquz7VWUxEy2qwJN8q/W66+l4AOi9DLnnAukJ0TzckDYpxbdfMv1zDQjV+s/YbqxInwUdpla1H48Q==",
+    //"requires": {
+      //"request": "^2.81.0",
+      //"request-promise": "^4.2.1"
+    //npm}
